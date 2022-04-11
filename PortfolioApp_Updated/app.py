44e881ba734da98c2819a94ab317fbbaaf886d09
jackspurrier <96054890+jackspurrier@users.noreply.github.com>
2022-04-11 01:47:34 +0100

    Update app.py

--- PortfolioApp_Updated/app.py
+++ PortfolioApp_Updated/app.py
@@ -816,4 +816,4 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
 
-    app.run(host='127.0.0.1', port=5000, debug=True,)
+    app.run(host='34.89.109.10', port=5000, debug=True,)
